fca863301ec509fc94df24ff5cb721c42086eb35
https://github.com/AmitAKY <pdeadly1@gmail.com>
2024-01-24 00:30:47 +0530

    datattypes and variables

---
+++ lesson1.js
@@ -0,0 +1,9 @@
+// // const accounId = 12233445;
+// // let accountEmail = "hii@gmail.com";
+// // var accountPassword = "124";
+// // accountCity = "jaipur";
+
+// // accounId = 2;
+// // console.log(accounId);
+// // console.table([accounId, accountEmail, accountPassword, accountCity]);
+//  prefeer not to use var due to block scope issue
---
+++ lesson2.js
@@ -0,0 +1,21 @@
+console.log("hiii");
+("use strict"); //treat all js code as a newwe version
+// alert("hello");//we are using nodejs,not browser
+
+console.log(3 + 3);
+console.log("hitesh");
+let name = "amit";
+let age = 18;
+let isLogggedin = false;
+// number=2 to power of 53
+//bigint
+//string=""
+//boolean=true,false
+//null-standalone value
+//undefined
+//symbol- will be used in react it is unique
+
+//object
+
+console.log(typeof undefined); //output is undefined
+console.log(typeof null); //output is object
