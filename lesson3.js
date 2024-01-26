0a3fb163281f8f4df9631dd4ce5904cd0be0acd4
https://github.com/AmitAKY <pdeadly1@gmail.com>
2024-01-26 22:15:24 +0530

    new concepts

---
+++ lesson3.js
@@ -0,0 +1,18 @@
+let score = "amit";
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"  can easily be converted to  number
+// "33abc =Nan"
+
+let isLoggedIn = 1;
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+// 1=>true"=>false
+// "hitesh"=>true
+let Number = 33;
+console.log(number);
+let stringNumber = String(Numner);
+console.log(stringNumber);
